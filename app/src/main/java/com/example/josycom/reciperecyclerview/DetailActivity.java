cfda8e73e21b0e7d5278c01e83f197f622757a6d
MayorJay <joseolu4gsm@yahoo.com>
2019-08-31 00:59:23 +0100

    Bug related to the ArrayList fixed. The App no more crashes

--- app/src/main/java/com/example/josycom/reciperecyclerview/DetailActivity.java
+++ app/src/main/java/com/example/josycom/reciperecyclerview/DetailActivity.java
@@ -17,6 +17,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         imageView = findViewById(R.id.recipe_image);
         textView = findViewById(R.id.recipe_details);
+        //Gets the data that is sent to it from the Main Activity and displays it
         extras = getIntent().getExtras();
         if (extras != null){
             String details = extras.getString("details");
